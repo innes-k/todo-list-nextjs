5035b24796e1f85a030c4eb6d41f40f29276a044
innes-k <innes.kwak@gmail.com>
2024-03-13 22:14:47 +0900

    feat: api - get company info

---
+++ src/app/api/company/route.ts
@@ -0,0 +1,20 @@
+export async function GET(request: Request) {
+  const response = await fetch("http://localhost:4005/companyInfo");
+  const info = await response.json();
+
+  if (!info) {
+    return new Response("Company Information is not found", {
+      status: 404,
+    });
+  }
+
+  const { name, desctiption, image } = info;
+
+  return Response.json({
+    companyInfo: {
+      name,
+      desctiption,
+      image,
+    },
+  });
+}
